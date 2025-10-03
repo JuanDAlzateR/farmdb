aaf58f98112a32ba6b49b51a1c9ce2a7e2ba9f2d
JuanDAlzateR <219579616+JuanDAlzateR@users.noreply.github.com>
2025-10-03 10:55:24 -0500

    added maven and restructure of project

---
+++ src/main/java/com/solvd/farm/Main.java
@@ -0,0 +1,24 @@
+package com.solvd.farm;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+//TIP To <b>interfaces.Run</b> code, press <shortcut actionId="interfaces.Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static final Logger LOGGER = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+
+
+    }
+
+    /* This method is just an example of the functionality in the code.
+    It can be called in the main menu as EXAMPLE: TEST FARM METHODS
+     */
+
+}
