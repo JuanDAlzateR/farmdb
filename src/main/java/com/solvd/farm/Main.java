5529f8b6078d4180d8c55b423453cc43f8ccc5fd
JuanDAlzateR <219579616+JuanDAlzateR@users.noreply.github.com>
2025-11-25 12:37:38 -0500

    added log4j slf4j binding

--- src/main/java/com/solvd/farm/Main.java
+++ src/main/java/com/solvd/farm/Main.java
@@ -13,7 +13,6 @@ public class Main {
     public static final Logger LOGGER = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
-
         //Services for DAO
         useServices();
 
